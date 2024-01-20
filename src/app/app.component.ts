4cc08c871b04e4153afd60b23eca35ba5069d4ae
michelealfonso <117176063+michelealfonso@users.noreply.github.com>
2024-01-20 15:04:17 +0100

    fix: imrpovements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rock-paper-scissors';
+
+  userScore = 0;
+  pcScore = 0;
+  userSelected!: string;
+  pcSelected!: string;
+
+  choices = ['rock', 'paper', 'scissors'];
+
+  play(userWeapon: string): void {
+    this.userSelected = userWeapon;
+    console.log( this.userSelected);
+    setTimeout(() => {
+      const randomNum = Math.floor(Math.random() * 3)
+      this.pcSelected = this.choices[randomNum];
+      console.log( this.choices);
+      this.checkResult();
+    }, 1000)
+  }
+
+  clearField() {
+    setTimeout(() => {
+      this.userSelected = '';
+      this.pcSelected = '';
+    }, 1500);
+  }
+
+  win(user: any, pc: any) {
+    this.userScore++;
+    this.userSelected = user;
+    this.pcSelected = pc;
+    this.clearField();
+  }
+
+  lose(user: any, pc: any) {
+    this.pcScore++;
+    this.userSelected = user;
+    this.pcSelected = pc;
+    this.clearField();
+  }
+
+  draw(user: any, pc: any) {
+    this.userSelected = user;
+    this.pcSelected = pc;
+    this.clearField();
+  }
+
+  checkResult() {
+    const userChoice = this.userSelected;
+    const pcChoice = this.pcSelected;
+
+    if (userChoice + pcChoice == 'rockscissors' || userChoice + pcChoice == 'paperrock' || userChoice + pcChoice == 'scissorspaper') {
+      this.win(userChoice, pcChoice);
+    }
+    else if ( userChoice + pcChoice == 'rockpaper' || userChoice + pcChoice == 'scissorsrock' || userChoice + pcChoice == 'paperscissors') {
+        this.lose(userChoice, pcChoice);
+    }
+    else {
+      this.draw(userChoice, pcChoice);
+    }
+  }
+
+  reset() {
+    this.pcScore = 0;
+    this.userScore = 0;
+  }
 }
